1234fd8551b9ea2e2190e67492fca673799eaaa1
Peter Lind <peter.e.lind@gmail.com>
2016-11-29 09:46:19 +0100

    using better node/npm/n install script

--- node/install.sh
+++ node/install.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 
-TARGET=`curl -s https://nodejs.org/en/download/current/ | grep linux-x64 | sed 's/^.*https/https/' | sed 's/".*$//'`
-FILE=`basename "$TARGET"`
-wget $TARGET
-tar -xvf $FILE
-
-DIR=`ls -l | grep '^d' | awk '{print $9}'`
-
-sudo cp $DIR/bin/node /usr/local/bin/
-
-mkdir ~/.npm-global
-$DIR/bin/npm config set prefix '~/.npm-global'
-$DIR/bin/npm install -g npm
-
-rm -rf $DIR
-rm $FILE
-
-npm -g i n
-sudo ~/.npm-global/bin/n stable
+curl -s -L https://git.io/n-install | bash -s -- -y
