be64fb3b859e6b3bdcb5dafe24e4e1773c6db49c
Peter Lind <peter.e.lind@gmail.com>
2019-09-25 22:12:19 +0200

    fixing buggy install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
 # install base apps
-sudo apt install terminator fonts-powerline zsh vim tmux git
+OLDPWD=`pwd`
+WD=$(realpath  $(dirname "$0")/..)
+cd $WD
+
+sudo apt install terminator fonts-powerline zsh vim tmux git curl wget
+
+sudo usermod -s $(which zsh) $(whoami)
 
 # install antigen for zsh
-curl -L git.io/antigen > antigen.zsh
+curl -L git.io/antigen > ~/antigen.zsh
 
-# install node through n
-curl -L https://git.io/n-install | bash
 
 # symlink files
-WD=$(realpath $(dirname $(dirname "$0")))
-
 ln -s $WD/git/.gitconfig $HOME/.gitconfig
 ln -s $WD/zsh/.zshrc $HOME/.zshrc
 ln -s $WD/tmux/.tmux.conf $HOME/.tmux.conf
 ln -s $WD/vim/.vimrc $HOME/.vimrc
 ln -s $WD/vim/.vim $HOME/.vim
 
+# install node through n
+curl -L https://git.io/n-install | zsh
+
 # create folders for vim sensible settings
 mkdir -p ~/.local/share/vim/swap ~/.local/share/vim/backup ~/.local/share/vim/undo
 
 # install terminator profiles
-terminator/install.sh
+$WD/terminator/install.sh
+
+cd $OLDPWD
