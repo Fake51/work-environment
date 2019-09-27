b1ab9d1bdb94684b55d36cb9dbb6d11098354421
Peter Lind <peter.e.lind@gmail.com>
2019-09-27 20:57:48 +0200

    adding Hack font

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,18 +12,22 @@ sudo usermod -s $(which zsh) $(whoami)
 curl -L git.io/antigen > ~/antigen.zsh
 
 
-# symlink files
+# symlink files and folders
 ln -s $WD/git/.gitconfig $HOME/.gitconfig
 ln -s $WD/zsh/.zshrc $HOME/.zshrc
 ln -s $WD/tmux/.tmux.conf $HOME/.tmux.conf
 ln -s $WD/vim/.vimrc $HOME/.vimrc
 ln -s $WD/vim/.vim $HOME/.vim
+ln -s $WD/fonts $HOME/.fonts
 
 # install node through n
 curl -L https://git.io/n-install | zsh
 
 # create folders for vim sensible settings
 mkdir -p ~/.local/share/vim/swap ~/.local/share/vim/backup ~/.local/share/vim/undo
+
+# update fonts
+fc-cache -rv
 
 # install terminator profiles
 $WD/terminator/install.sh
