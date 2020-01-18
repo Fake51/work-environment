2c3967db6eae7b69b3d091ed3187fdc0369c3764
Peter Lind <peter.e.lind@gmail.com>
2020-01-18 21:30:52 +0100

    adding php to install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,14 @@ OLDPWD=`pwd`
 WD=$(realpath  $(dirname "$0")/..)
 cd $WD
 
+sudo apt -y install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+
+PHP_VERSION=`apt-cache search php | grep '^php[0-9.]\+ ' | sort -r | head -n 1 | cut -d ' ' -f 1`
+
 sudo apt install -y terminator fonts-powerline zsh vim tmux git curl wget gnupg2 keepassxc elixir vim-doc
+sudo apt install -y php$PHP_VERSION php-xdebug php$PHP_VERSION-bz2 php$PHP_VERSION-cli php$PHP_VERSION-fpm php$PHP_VERSION-curl php$PHP_VERSION-intl php$PHP_VERSION-json php$PHP_VERSION-mysql php$PHP_VERSION-opcache php$PHP_VERSION-sqlite3 php$PHP_VERSION-tidy php$PHP_VERSION-zip php$PHP_VERSION-readline php-yaml
 
 # install antigen for zsh
 curl -L git.io/antigen > ~/antigen.zsh
