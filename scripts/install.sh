c15197f255e8149504d6cc5b3e224c9045a758a1
Peter Lind <peter.e.lind@gmail.com>
2021-01-04 22:13:24 +0100

    improving install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,41 +11,65 @@ create_link() {
 }
 
 # install base apps and dependencies
+echo
 echo "Installing packages"
 if ! sudo apt install -y \
   autoconf \
+  bison \
   build-essential \
   curl \
   dirmngr \
   fonts-powerline \
+  gettext \
   git \
   gnupg2 \
   keepassxc \
+  libcurl4-openssl-dev \
+  libedit-dev \
+  libgd-dev \
+  libicu-dev \
+  libjpeg-dev \
+  libmysqlclient-dev \
   libncurses-dev \
   libncurses5-dev \
+  libonig-dev \
+  libpng-dev \
+  libpq-dev \
+  libreadline-dev \
+  libsqlite3-dev \
   libssh-dev \
+  libssl-dev \
+  libxml2-dev \
   libxml2-utils \
+  libzip-dev \
   m4 \
   openjdk-11-jdk \
+  openssl \
+  pkg-config \
+  re2c \
   software-properties-common \
   terminator \
   tmux \
   unixodbc-dev \
   unzip \
   vim \
-  vim-doc \
   wget \
+  zlib1g-dev \
   zsh
 then
   echo "Aborting - failed to install packages"
   exit 1
 fi
 
+echo
+
 # install antigen for zsh
 if [ ! -f ~/antigen.zsh ]
 then
   echo "Installing Antigen for zsh"
   curl -L git.io/antigen > ~/antigen.zsh
+else
+  echo "Antigen in place, skipping"
 fi
 
 # symlink files and folders
@@ -56,33 +80,46 @@ create_link $WD/vim/.vimrc $HOME/.vimrc
 create_link $WD/vim/.vim $HOME/.vim
 create_link $WD/fonts $HOME/.fonts
 
+echo
 STACK=`which stack`
 if [ -z "$STACK" ]
 then
     # install haskell stack
     echo "Installing haskell stack"
     curl -sSL https://get.haskellstack.org/ | sh
+else
+  echo "Haskell in place, skipping"
 fi
 
 # create folders for vim sensible settings
 mkdir -p ~/.local/share/vim/swap ~/.local/share/vim/backup ~/.local/share/vim/undo
 
 # install asdf
-if [  -d ~/.asdf ]
+if [ ! -d ~/.asdf ]
 then
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf
   cd ~/.asdf
   git checkout "$(git describe --abbrev=0 --tags)"
 
   . $HOME/.asdf/asdf.sh
+fi
 
-  # nodejs
+echo
+if ! asdf list nodejs > /dev/null
+then
+  echo "Installing NodeJS"
   asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
   bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
   asdf install nodejs latest
   asdf global nodejs $(asdf latest nodejs)
+else
+  echo "NodeJS in place, skipping"
+fi
 
-  # erlang & elixir
+echo
+if ! asdf list elixir > /dev/null
+then
+  echo "Installing Erlang & Elixir"
   asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
   asdf install erlang latest
   asdf global erlang $(asdf latest erlang)
@@ -90,17 +127,39 @@ then
   asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
   asdf install elixir latest
   asdf global elixir $(asdf latest elixir)
+else
+  echo "Elixir in place, skipping"
 fi
 
-# update fonts
-fc-cache -rv
+echo
+if ! asdf list php > /dev/null
+then
+  asdf plugin-add php https://github.com/asdf-community/asdf-php.git
+  VERSION=$(asdf list all php | grep -vi rc | grep -v alpha | grep -v beta | tail -n 1)
+  echo "Installing PHP version $VERSION"
+  asdf install php $VERSION
+  asdf global php $VERSION
+else
+  echo "PHP in place, skipping"
+fi
+
+echo
+echo "Updating fonts"
+fc-cache -rv >/dev/null
 
 # set shell
+echo
 echo "Setting zsh as shell"
 sudo usermod -s $(which zsh) $(whoami)
 
 # install terminator profiles
-#$WD/terminator/install.sh
+TERMINATOR_CONFIG="~/.config/terminator/"
+
+if [ ! -d $DESTINATION ]
+then
+  mkdir -p $DESTINATION
+  cp $WD/terminator/config $DESTINATION
+fi
 
 sed -i 's/https:\/\/github.com\//git@github.com:/' .git/config
 
